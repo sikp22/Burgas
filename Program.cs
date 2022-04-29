e0497b84940ad2ff563904323d2d17dbf8ece9e9
Sasha Petrova <sikp22@gmail.com>
2022-04-29 09:55:50 +0300

    Add project

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc zad = new Calc();
+            bool end = false;
+
+            while (!end)
+            {
+
+                Console.Write("Ender first number:");
+                zad.num1 = double.Parse(Console.ReadLine());
+                Console.Write("Ender second number :");
+                zad.num2 = double.Parse(Console.ReadLine());
+                Console.Write("Ender operation :");
+                zad.oper = char.Parse(Console.ReadLine());
+               // zad.getResult(zad.num1, zad.num2, zad.oper);
+                Console.WriteLine("result: {0}", zad.getResult(zad.num1, zad.num2, zad.oper));
+                if (Console.ReadLine() == "end") end = true;
+
+
+               
+
+
+            }
+        }
+    }
+}
---
+++ calc.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Calc
+    {
+        public double num1;
+        public double num2;
+        public char oper;
+
+        //public char Oper
+        //{
+        //    get { return oper; }
+        //    set { oper = value; }
+        //}
+
+        //public double Num2
+        //{
+        //    get { return num2; }
+        //    set { num2 = value; }
+        //}
+
+
+        //public double Num1      
+        //{
+        //    get { return num1; }
+        //    set { num1 = value; }
+        //}
+
+        //public calc(double num1, double num2, char oper)
+        //{
+        //    this.Num1 = num1;
+        //    this.Num2 = num2;
+        //    this.oper = Oper;
+        //}
+
+
+
+         public  double getResult(double num1,double num2, char oper)
+        {
+             double result = double.NaN;
+            switch (oper)
+            {
+                case '+':result = num1 + num2; break;
+                case '*': result = num1 * num2; break;
+                case '-': result = num1 - num2; break;
+                case '/':
+                    {
+                        if (num2!=0)
+                        {
+                        result = num1 / num2;
+                        }
+                       
+                    
+                    }  break;
+
+                default:
+                    break;
+            }
+            return result;
+        }
+
+
+    }
+}
